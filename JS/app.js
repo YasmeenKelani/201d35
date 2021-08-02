92590ba4670db43cc984e9e9cc144f99bbe15ee4
YasmeenKelani <yasminekailani98@gmail.com>
2021-08-01 23:14:32 -0700

    project

---
+++ JS/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let name =prompt("what is your name?");
+alert(" Thinking about choosing a super type of colors?, I'm glad to help you welcome to our website! " +name);
+console.log(name)
+
+let age = Number(prompt('How old are you?'))
+alert(age)
+console.log(age);
+
+let interest = prompt('Are you intresting in colors?')
+alert(interest)
+console.log(interest);
+
+let colors = prompt('What is your favorite type of colors to paint?')
+alert(colors)
+console.log(colors);
